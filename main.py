81bfa1718654f6ab120fab496d97ee4e1b060c7f
David Nickerson <david.nickerson@gmail.com>
2024-10-17 00:13:34 +1300

    update to first generate list of tags without geolocating

--- main.py
+++ main.py
@@ -18,48 +18,54 @@ def _parse_args():
     return parser.parse_args()
 
 
+def _get_location_tags(cursor, pid, ancestor_tags):
+    # find all tags that have the given parent ID
+    cursor.execute("SELECT * FROM TAGS WHERE PID = ?", (pid,))
+
+    # Fetch all rows that match the PID value
+    rows = cursor.fetchall()
+    tags = []
+    #geolocator = Nominatim(user_agent="andres_digikam_migration_script")
+    for row in rows:
+        id = row[0]
+        tag = row[2]
+        place_name = tag
+        for ancestor_tag in ancestor_tags:
+            if ancestor_tag == 'North Island':
+                ancestor_tag = ''
+            elif ancestor_tag == 'South Island':
+                ancestor_tag = ''
+            place_name += ', ' + ancestor_tag
+        # Get the location details for the given place name
+        #location = geolocator.geocode(place_name)
+        tags.append({
+            'id': id,
+            'name': tag,
+            'place_name': place_name,
+            'latitude': None,
+            'longitude': None
+        })
+
+        ancestors = [tag] + ancestor_tags
+        tags.extend(_get_location_tags(cursor, id, ancestors))
+
+    return tags
+
+
+
 def get_location_tags(conn):
     # Create a cursor object to interact with the database
     cursor = conn.cursor()
 
+    # get the ID of our root "Places" tag
     cursor.execute('SELECT id FROM Tags Where name = "Places"')
     places_root_id = cursor.fetchone()[0]
     print(f'Root places ID: {places_root_id}')
 
-    cursor.execute("SELECT * FROM TAGS WHERE PID = ?", (places_root_id,))
+    # recursively build our locations array
+    location_tags = _get_location_tags(cursor, places_root_id, [])
 
-    # Fetch all rows that match the PID value
-    rows = cursor.fetchall()
-
-    # Print each row that matches the PID value
-    locations = []
-    geolocator = Nominatim(user_agent="andres_digikam_migration_script")
-    for row in rows:
-        place_name = row[2]
-        # Get the location details for the given place name
-        location = geolocator.geocode(place_name)
-
-        # Print the latitude and longitude
-        if location:
-            locations.append({
-                'id': row[0],
-                'name': place_name,
-                'latitude': location.latitude,
-                'longitude': location.longitude
-            })
-            print(f"Latitude: {location.latitude}, Longitude: {location.longitude}")
-        else:
-            print("Location not found.")
-
-        if place_name == 'New Zealand':
-            # Get the location details for the given place name
-            location = geolocator.geocode(place_name + ', Auckland')
-            print(f"Latitude: {location.latitude}, Longitude: {location.longitude}")
-        else:
-            print("Auckland not found.")
-
-
-    return locations
+    return location_tags
 
 
 if __name__ == "__main__":
