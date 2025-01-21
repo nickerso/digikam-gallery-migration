92d33af71e300248733d28676a3e20fa74c87c44
David Nickerson <david.nickerson@gmail.com>
2025-01-21 23:02:47 +1300

    updates for retrieving and caching locations from place tags in the digikam database.

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def _get_location_tags(cursor, pid, ancestor_tags):
     # Fetch all rows that match the PID value
     rows = cursor.fetchall()
     tags = []
-    #geolocator = Nominatim(user_agent="andres_digikam_migration_script")
     for row in rows:
         id = row[0]
         tag = row[2]
@@ -36,8 +35,6 @@ def _get_location_tags(cursor, pid, ancestor_tags):
             elif ancestor_tag == 'South Island':
                 ancestor_tag = ''
             place_name += ', ' + ancestor_tag
-        # Get the location details for the given place name
-        #location = geolocator.geocode(place_name)
         tags.append({
             'id': id,
             'name': tag,
@@ -50,7 +47,6 @@ def _get_location_tags(cursor, pid, ancestor_tags):
         tags.extend(_get_location_tags(cursor, id, ancestors))
 
     return tags
-
 
 
 def get_location_tags(conn):
@@ -66,6 +62,28 @@ def get_location_tags(conn):
     location_tags = _get_location_tags(cursor, places_root_id, [])
 
     return location_tags
+
+
+class Location:
+    def __init__(self, place, latitude, longitude):
+        self.place = place
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f"Location(place={self.place}, latitude={self.latitude}, longitude={self.longitude})"
+
+    def __str__(self):
+        return f"Place: {self.place}, Latitude: {self.latitude}, Longitude: {self.longitude}"
+
+
+def get_location(geolocator, place_name):
+    # Get the location details for the given place name
+    location = geolocator.geocode(place_name)
+    if location is None:
+        print(f'Unable to get_location for the place: {place_name}')
+        location = Location(place_name, None, None)
+    return location
 
 
 if __name__ == "__main__":
@@ -84,6 +102,36 @@ if __name__ == "__main__":
     else:
         with open(args.location_cache, 'r') as f:
             location_information = json.load(f)
+
+    # need to make sure locations have location coordinates
+    geolocator = None
+    # want to cache locations to make sure we don't exceed limits on geocode lookup
+    LOCATION_MAP = {}
+    location_updated = False
+    for location in location_information:
+        place = location['place_name']
+        if place not in LOCATION_MAP:
+            if (location['latitude'] is None) or (location['longitude'] is None):
+                print(f'Need to retrieve location for the place: {place}')
+                if geolocator is None:
+                    geolocator = Nominatim(user_agent="andres_digikam_migration_script")
+                loc = get_location(geolocator, place)
+                location['latitude'] = loc.latitude
+                location['longitude'] = loc.longitude
+                location_updated = True
+            LOCATION_MAP[place] = location
+        else:
+            cached_location = LOCATION_MAP[place]
+            if (location['latitude'] is None) or (location['longitude'] is None):
+                location['latitude'] = cached_location['latitude']
+                location['longitude'] = cached_location['longitude']
+
+    if location_updated:
+        # update the cache file
+        with open(args.location_cache, 'w', encoding='utf-8') as f:
+            json.dump(location_information, f, ensure_ascii=False, indent=4)
+
+
 
     connection.close()
     if args.dry_run:
