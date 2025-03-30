9353878c7f0e3047828687bcd8f568a9b09b2ccf
David Nickerson <david.nickerson@gmail.com>
2025-03-30 23:01:28 +1300

    Update to make use of cached location information

--- explore-tags.py
+++ explore-tags.py
@@ -1,6 +1,5 @@
 import argparse
 import sqlite3
-from geopy.geocoders import Nominatim
 import json
 
 
@@ -11,6 +10,11 @@ def _parse_args():
                         default='digikam4.db')
     parser.add_argument('-r', "--recursive", help="List the tag hierarchy",
                         action="store_true")
+    parser.add_argument("--location-cache", help="JSON file with the location information",
+                        default="locations.json")
+    parser.add_argument("--cache-locations", help="Add the tags as locations to the location cache",
+                        action="store_true")
+
     return parser.parse_args()
 
 
@@ -45,6 +49,46 @@ def print_tag(tag, recursive, conn):
     return
 
 
+def _get_tag_location(cursor, pid, ancestor_tags):
+    # find all tags that have the given parent ID
+    cursor.execute("SELECT * FROM TAGS WHERE PID = ?", (pid,))
+
+    # Fetch all rows that match the PID value
+    rows = cursor.fetchall()
+    tags = []
+    for row in rows:
+        id = row[0]
+        tag = row[2]
+        place_name = tag
+        for ancestor_tag in ancestor_tags:
+            place_name += ', ' + ancestor_tag
+        tags.append({
+            'id': id,
+            'name': tag,
+            'place_name': place_name,
+            'latitude': None,
+            'longitude': None
+        })
+
+        ancestors = [tag] + ancestor_tags
+        tags.extend(_get_tag_location(cursor, id, ancestors))
+
+    return tags
+
+
+def get_tags_as_locations(tag, conn):
+    # Create a cursor object to interact with the database
+    cursor = conn.cursor()
+
+    # get the ID of our tag
+    cursor.execute(f'SELECT id FROM Tags Where name = "{tag}"')
+    root_id = cursor.fetchone()[0]
+    # recursively build our tag array
+    locations = _get_tag_location(cursor, root_id, [])
+
+    return locations
+
+
 def get_toplevel_tags(conn):
     # Create a cursor object to interact with the database
     cursor = conn.cursor()
@@ -74,5 +118,12 @@ if __name__ == "__main__":
             print(f'- {tag}')
     else:
         print_tag(args.tag, args.recursive, connection)
+        if args.cache_locations:
+            print('caching locations')
+            with open(args.location_cache, 'r') as f:
+                location_information = json.load(f)
+            location_information.extend(get_tags_as_locations(args.tag, connection))
+            with open(args.location_cache, 'w', encoding='utf-8') as f:
+                json.dump(location_information, f, ensure_ascii=False, indent=4)
 
     connection.close()
